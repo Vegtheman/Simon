78d5f16d48b2c13ddb8a2fca3b3975bd6da76a1f
Vegtheman <97992571+Vegtheman@users.noreply.github.com>
2022-05-18 11:19:09 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,22 +1,43 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let start= false;
 
+$(".btn").click(function() {
+
+  let userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+});
 
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColor = buttonColors[randomNumber];
+
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
 
-  $("#" + randomChosenColor).on("click", function() {
+}
 
-    $("." + randomChosenColor).fadeOut(100).fadeIn(100);
+function playSound(name) {
 
-    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-    audio.play();
-    return randomChosenColor;
-  });
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
 
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
 
-};
+$(document).on("keypress", function () {
 
-$(".btn").click(nextSequence);
+$("#level-title").text("Level " + level);
+nextSequence()
+start = true;
+});
